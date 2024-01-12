adae36ea888d2f116f88fe5c2e20d3d41e117ae8
Papa Abdoulaye NDIAYE <pandiaye4>
2024-01-12 15:32:09 +0100

    chenillard

---
+++ lab1/lab1_bb.v
@@ -0,0 +1,12 @@
+
+module lab1 (
+	clk_clk,
+	led_export,
+	reset_reset_n,
+	bouton_export);	
+
+	input		clk_clk;
+	output	[7:0]	led_export;
+	input		reset_reset_n;
+	input		bouton_export;
+endmodule
---
+++ lab1/lab1_inst.vhd
@@ -0,0 +1,17 @@
+	component lab1 is
+		port (
+			clk_clk       : in  std_logic                    := 'X'; -- clk
+			led_export    : out std_logic_vector(7 downto 0);        -- export
+			reset_reset_n : in  std_logic                    := 'X'; -- reset_n
+			bouton_export : in  std_logic                    := 'X'  -- export
+		);
+	end component lab1;
+
+	u0 : component lab1
+		port map (
+			clk_clk       => CONNECTED_TO_clk_clk,       --    clk.clk
+			led_export    => CONNECTED_TO_led_export,    --    led.export
+			reset_reset_n => CONNECTED_TO_reset_reset_n, --  reset.reset_n
+			bouton_export => CONNECTED_TO_bouton_export  -- bouton.export
+		);
+
